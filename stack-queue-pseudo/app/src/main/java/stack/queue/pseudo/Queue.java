621817077f9e7d56c003acf6c4ba4b1ae52121db
mohammadmu1 <mbn.1991@yahoo.com>
2023-09-04 11:12:02 +0300

    stq

---
+++ stack-queue-pseudo/app/src/main/java/stack/queue/pseudo/Queue.java
@@ -0,0 +1,71 @@
+import java.util.Stack;
+
+public class PseudoQueue<T> {
+    private Stack<T> stack1;
+    private Stack<T> stack2;
+
+    public PseudoQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+
+    public void enqueue(T val) {
+        stack1.push(vall);
+    }
+
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+
+        if (stack2.isEmpty()) {
+
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        T ans = stack2.pop();
+
+
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+
+        return ans;
+    }
+
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+
+        if (stack2.isEmpty()) {
+
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        T ans = stack2.peek();
+
+
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+
+        return ans;
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty();
+    }
+
+    public int size() {
+        return stack1.size();
+    }
+
+}
