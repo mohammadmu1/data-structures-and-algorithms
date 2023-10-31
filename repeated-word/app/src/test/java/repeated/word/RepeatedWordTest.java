77dcec5a92d483e7271789ce9acb55475c75dd74
mohammadmu1 <mbn.1991@yahoo.com>
2023-10-31 13:34:27 +0300

    31

---
+++ repeated-word/app/src/test/java/repeated/word/RepeatedWordTest.java
@@ -0,0 +1,34 @@
+package repeated.word;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RepeatedWordTest {
+
+
+ 
+
+    @Test
+    public void testNoRepeatedWord() {
+        String input = "No repeated words in this input";
+        String result = RepeatedWord.repeatedWord(input);
+
+        assertEquals("There is no repeated string", result);
+    }
+
+    @Test
+    public void testRepeatedWordAtBeginning() {
+        String input = "word word word repeated.";
+        String result = RepeatedWord.repeatedWord(input);
+
+        assertEquals("word", result);
+    }
+
+    @Test
+    public void testRepeatedWordAtEnd() {
+        String input = "This is the end, my only friend, the end.";
+        String result = RepeatedWord.repeatedWord(input);
+
+        assertEquals("the", result);
+    }
+}
